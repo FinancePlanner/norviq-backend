5c618bfb20124d806344a40a6615c9daa226f5bb
FACorreiaa <fernandocorreia316@gmail.com>
2026-06-26 15:57:46 +0100

    update cicd

--- Sources/StockPlanBackend/Shared/StockPlanShared+Content.swift
+++ Sources/StockPlanBackend/Shared/StockPlanShared+Content.swift
@@ -60,6 +60,9 @@ extension DashboardAllocationDTO: @retroactive Content {}
 
 extension PortfolioSummaryResponse: @retroactive Content {}
 extension PortfolioPerformanceResponse: @retroactive Content {}
+extension PortfolioDividendsResponse: @retroactive Content {}
+extension DividendProjectedItem: @retroactive Content {}
+extension DividendMonthlyBreakdown: @retroactive Content {}
 extension TransactionResponse: @retroactive Content {}
 extension LotResponse: @retroactive Content {}
 extension PnlResponse: @retroactive Content {}
